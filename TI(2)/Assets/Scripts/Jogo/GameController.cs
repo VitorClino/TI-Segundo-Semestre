5c7f935d39ed91eaf900e3a467fa01d7779bc856
Vitor <128195570+VitorClino@users.noreply.github.com>
2023-09-14 20:44:16 -0300

    Correção da Loja
    ---

--- TI(2)/Assets/Scripts/Jogo/GameController.cs
+++ TI(2)/Assets/Scripts/Jogo/GameController.cs
@@ -7,13 +7,22 @@ public class GameController : MonoBehaviour
     public GameObject[] player;
     private GameObject auxPlayer;
 
+    public static GameController instance;
+
     public Vector3 spawn;
-    
+
+    public int escolha = 0;
     void Start()
     {
+        instance = this;
         Time.timeScale = 1;
-        auxPlayer = Instantiate(player[0], spawn, transform.rotation);
+
+        auxPlayer = Instantiate(player[escolha], spawn, transform.rotation);
+
+        DontDestroyOnLoad(this.gameObject);
     }
+
+    
 
     public void CriarPlayer(int i)
     {
@@ -33,7 +42,12 @@ public class GameController : MonoBehaviour
             Destroy(auxPlayer);
             auxPlayer = Instantiate(player[j], spawn , transform.rotation );
         }
-        
+        if (auxPlayer.tag == "PlayerVermelho") escolha = 0;
+
+        if (auxPlayer.tag == "PlayerAmarelo" ) escolha = 1;
+
+
+        Debug.Log(escolha);
     }
     
 
